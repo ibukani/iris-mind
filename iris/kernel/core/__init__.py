af73509f0aa495cb5bb3cfabe4d2d0ea07495834
ibukani <ibukani2056@gmail.com>
2026-05-16 02:23:01 +0900

    Rename kernel_process.py to process.py

--- iris/kernel/core/__init__.py
+++ iris/kernel/core/__init__.py
@@ -1,6 +1,6 @@
 from iris.kernel.core.agent_kernel import AgentKernel, AnomalyDetector
 from iris.kernel.core.factory import KernelContext, KernelFactory
-from iris.kernel.core.kernel_process import KernelProcess, KernelProcessProtocol
+from iris.kernel.core.process import KernelProcess, KernelProcessProtocol
 
 __all__ = [
     "AgentKernel",
